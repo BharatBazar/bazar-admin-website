a8972228ac7524bac9a89700f30e8fc4d5c840fa
roneyyb <bothra.rajat08@gmail.com>
2021-10-14 11:54:23 +0530

    added fetch api for catalogue

--- src/container/dashboard/product/ProductStatus.tsx
+++ src/container/dashboard/product/ProductStatus.tsx
@@ -3,23 +3,25 @@ import * as React from 'react';
 
 import { errorShow } from '../../../components/ALert';
 import { formRequiredRule } from '../../../constants';
-import { getProductCatelogue } from '../../../server/catalogue/catalogue.api';
+import { getProductCatelogue, getProductCatelogueWithAncestors } from '../../../server/catalogue/catalogue.api';
 
 const { Option } = Select;
 
 interface ProductStatusProps {}
 
 const ProductStatus: React.FunctionComponent<ProductStatusProps> = () => {
-    const [loading, setLoading] = React.useState(false);
+    const [loading, setLoading] = React.useState(true);
     const [catalogue, setCatalogue] = React.useState([]);
+    const [form] = Form.useForm();
 
     // To load all categories from backend which are avaialable in the market
     const loadCatalogueFromServer = async () => {
         try {
-            const response = await getProductCatelogue({ subCategoryExist: false });
+            const response = await getProductCatelogueWithAncestors();
             setLoading(false);
             if (response.status === 1) {
-                //  setCategory(response.payload);
+                console.log(response);
+                setCatalogue(response.payload);
             }
         } catch (error) {
             setLoading(false);
@@ -35,13 +37,22 @@ const ProductStatus: React.FunctionComponent<ProductStatusProps> = () => {
     return (
         <div>
             <Card loading={loading} title={'Filter'}>
-                <Form.Item style={{ flex: 1 }} label="Classifier type :" name="type" rules={formRequiredRule}>
-                    <Select allowClear>
-                        {catalogue.map((classifier) => (
-                            <Option value={classifier}>{classifier}</Option>
-                        ))}
-                    </Select>
-                </Form.Item>
+                <Form
+                    form={form}
+                    labelCol={{ span: 4 }}
+                    wrapperCol={{ span: 14 }}
+                    name="basic"
+                    layout="horizontal"
+                    initialValues={{ remember: true }}
+                >
+                    <Form.Item style={{ flex: 1 }} label="Classifier type :" name="type" rules={formRequiredRule}>
+                        <Select allowClear>
+                            {catalogue.map((classifier) => (
+                                <Option value={classifier.name}>{classifier.name}</Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+                </Form>
             </Card>
         </div>
     );
